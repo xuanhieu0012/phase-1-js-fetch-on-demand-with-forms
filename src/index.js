ec428eb08296f49a58f99221092322084ce928fd
Hieu <xuanhieu0012@gmail.com>
2021-08-17 12:07:32 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 const init = () => {
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', e => {
+        e.preventDefault()
+        const input =e.target.children[1].value
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+            title.innerHTML = data.title;
+            summary.innerHTML = data.summary
+
+        })
+    })
   
 }
 
